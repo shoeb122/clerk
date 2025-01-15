65007d3377129dd4204cb75ef499d7e576708089
shoeb122 <160999309+shoeb122@users.noreply.github.com>
2025-01-14 21:59:23 -0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,16 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
+  console.log('Request URL:', request.url) // Add logging for debugging
   if (!isPublicRoute(request)) {
     await auth.protect()
   }
 })
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
